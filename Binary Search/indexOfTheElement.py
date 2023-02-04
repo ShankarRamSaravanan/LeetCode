643938bf458497b535977abdf8beca198464a837
ShankarRam Saravanan <sankarams18@gmail.com>
2023-02-04 16:44:41 -0500

    new commit

--- Binary Search/indexOfTheElement.py
+++ Binary Search/indexOfTheElement.py
@@ -1,19 +1,25 @@
-def indexOfElement(num,target):
+def indexOfElement(num, target):
 
-    num = num.sort()
+    left = 0
+    right = len(num) - 1
 
-    left =0
-    right =len(num) -1
+    while left <= right:
+
+        midpoint = left + right//2
+
+        if num[midpoint] > target:
+            right = midpoint - 1
+
+        elif num[midpoint] < target:
+            left = midpoint + 1
+
+        else:
+            return midpoint
+
+    else:
+        return -1
 
 
-
-
-    
-
-
-
-
-
-
-num =[199,5,6,88,13,11,78,2,5,78,4,9,2,4]
-print(indexOfElement(num))
+target = 78
+num = [-1, 0, 3, 5, 9, 12]
+print(indexOfElement(num, target))
